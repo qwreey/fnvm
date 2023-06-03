3014b0e3431f73c51ac4160d558aa7467145934a
qwreey75 <qwreey75@gmail.com>
2023-06-03 18:10:15 +0900

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,7 +7,8 @@ fnvm_escape_replace() {
 	echo "${REPLY%$'\n'}"
 }
 fnvm_replace() {
-	grep -F -q "$2" "$1" && sed -n -e ':a' -e '$!{N;ba' -e '}' -e "s/$(fnvm_escape_search "$2")/$(fnvm_escape_replace "$3")/p" -i "$1"
+	grep -F -q "$2" "$1" || return 1
+	sed -n -e ':a' -e '$!{N;ba' -e '}' -e "s/$(fnvm_escape_search "$2")/$(fnvm_escape_replace "$3")/p" -i "$1"
 }
 
 # update single rc file
@@ -17,15 +18,19 @@ fnvm_update_rcfile() {
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion'
 	pattern2='export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm'
+	pattern3="export NVM_DIR=\"$(cygpath -w $HOME)/.nvm\"
+[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"  # This loads nvm
+[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"  # This loads nvm bash_completion"
 	replace='# load fnvm
 source $HOME/.nvm/fnvm/fnvm.sh; fnvm_init'
 
 	grep -F -q 'source $HOME/.nvm/fnvm/fnvm.sh; fnvm_init' "$1" && return
-	grep -F -q "$pattern" "$1" || grep -F -q "$pattern2" "$1"
+	grep -F -q "$pattern" "$1" || grep -F -q "$pattern2" "$1" || grep -F -q "$pattern3"
 	if [ "$?" = "0" ]; then
 		echo "Updating $1 ..."
 		fnvm_replace "$1" "$pattern1" "$replace"
 		fnvm_replace "$1" "$pattern2" "$replace"
+		fnvm_replace "$1" "$pattern3" "$replace"
 	else
 		echo "'source nvm.sh' is not found from $1."
 		echo "Appending fnvm loader on end of $1"
