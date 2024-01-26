c63931881c5e0cfcebc6c4bfb4842590551d73a3
Qwreey <qwreey75@gmail.com>
2024-01-27 02:50:45 +0900

    Update fnvm.sh

--- fnvm.sh
+++ fnvm.sh
@@ -116,9 +116,9 @@ fnvm_update_rcfile() {
 [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"  # This loads nvm
 [ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"  # This loads nvm bash_completion"
 	replace='# load fnvm
-source $HOME/.nvm/fnvm/fnvm.sh; fnvm_init'
+source $FNVM_DIR/fnvm.sh; fnvm_init'
 
-	grep -F -q 'source $HOME/.nvm/fnvm/fnvm.sh; fnvm_init' "$1" && return
+	grep -F -q 'source $FNVM_DIR/fnvm.sh; fnvm_init' "$1" && return
 	content="$(cat "$1")"
 	fnvm_safe_find "$content" "# FNVM NOUPDATE" && return
 
